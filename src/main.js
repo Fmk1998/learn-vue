1ea5baab0bf3f0a101d451e241ce84eb4a2b87ce
Fmk1998 <1092911991@qq.com>
2019-11-18 13:09:15 +0800

    day2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// 数据请求
+import axios from 'axios'
+// 引入
+import ElementUI from 'element-ui';
+// 样式表
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
+
+// 挂载全局请求函数方法
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
